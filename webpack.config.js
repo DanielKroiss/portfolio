4eb9e66165426201547aeb04bec447ed7725a723
DanielKroiss <daniel_kroiss@gmx.at>
2024-01-07 11:06:55 +0100

    set up website with webpack and tailwind

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+        ],
+    },
+    devServer: {
+        watchFiles: ['src/**/*'],
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "src/index.html", to: "index.html" },
+            ],
+        })
+    ]
+};
